92e145e7149ed1d736c3375ca83ef5741cdf2b48
Pedro Contrears Avendano <pedro.contreras.avendano@gmail.com>
2021-03-03 15:38:16 -0500

    Move CSS into its own file

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import * as CSS from 'csstype';
 import { InteractiveLink } from './InteractiveLink';
 import { Switch, Route } from 'react-router-dom';
