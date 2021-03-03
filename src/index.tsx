26f3850afb23cb0d5bc7890782f4112463309f5f
Pedro Contrears Avendano <pedro.contreras.avendano@gmail.com>
2021-03-03 17:48:58 -0500

    Remove script for GitHub Pages

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './components/App';
+import './resources/styles.css';
 
 ReactDOM.render(
   <BrowserRouter basename="/cafecito">
